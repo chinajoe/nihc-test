f3bc124d2ea7741da990b13de9b887514106ce89
chinajoe39 <chinajoe39@outlook.com>
2015-12-15 23:49:13 +0800

    edit third

--- TestGit/src/com/soft/entity/Test.java
+++ TestGit/src/com/soft/entity/Test.java
@@ -5,6 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello git");
 		System.out.println("edit second");
+		System.out.println("edit third");
 	}
 	
 }
